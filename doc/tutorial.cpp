ac8d806564e4faaf6e7c83de2e177854d9026e6f
richwareham <richwareham@625a9de0-492d-0410-920c-a7e632af1907>
2002-08-02 07:48:21 +0000

    Small addition to docs
    git-svn-id: svn://svn.mplayerhq.hu/dvdnav/trunk/libdvdnav@515 625a9de0-492d-0410-920c-a7e632af1907

--- doc/tutorial.cpp
+++ doc/tutorial.cpp
@@ -146,186 +146,108 @@ Return to the \ref tutorial.
 
 /*! \page firstprog A first libdvdnav program
 
+\section compiling Compiling a libdvdnav program
+
+Below is a simple <tt>libdvdnav</tt> program. Type/copy it and save it
+into the file 'dvdtest.c'.
+
 \verbatim
+#include <stdio.h>
+#include <dvdnav/dvdnav.h>
+#include <dvdnav/dvdnav_events.h>
+#include <sys/types.h>
+
 int main(int argc, char **argv) {
-  
-  printf("Opening DVD...\n");
+  dvdnav_t *dvdnav;
+  int finished, len, event;
+  uint8_t buf[2050];
+ 
+  /* Open the DVD */
   dvdnav_open(&dvdnav, "/dev/dvd");
 
-  region = 0; dump = 0; tt_dump = -2;
-  CALL(dvdnav_get_region_mask(dvdnav, &region));
-  printf("The DVD can be played in the following regions: ");
-  for(i=0; i<8; i++) {
-    if(region & (1<<i)) {
-      printf("%i ", i+1);
-    }
-  }
-  printf("\n");
-
-  printf("Reading...\n");
+  fprintf(stderr, "Reading...\n");
   finished = 0;
-  while(!finished) {
+  while(!finished) {  
     int result = dvdnav_get_next_block(dvdnav, buf,
-				       &event, &len);
+                                       &event, &len);
 
     if(result == DVDNAV_STATUS_ERR) {
       fprintf(stderr, "Error getting next block (%s)\n",
-	      dvdnav_err_to_string(dvdnav));
-      assert(0);
+              dvdnav_err_to_string(dvdnav));
+      exit(1);
     }
 
     switch(event) {
      case DVDNAV_BLOCK_OK:
-      
-      if((output_no == 0) || (change_output)) {
-	output_no++;
-	output_size = 0;
-	change_output = 0;
-	printf("Opening output...\n");
-	snprintf(filename, 255, "out_%03i.mpeg", output_no);
-	if(output_fd != -1) {
-	  close(output_fd);
-	}
-	output_fd = open(filename, O_CREAT | O_WRONLY, S_IRWXU | S_IRWXG);
-	if(output_fd == -1) {
-	  printf("Error opening output\n");
-	  assert(0);
-	}
-      }
-      
-      if(dump || (tt_dump != -2)) {
-	write(output_fd, buf, len);
-	output_size += (unsigned long)len;
-      }
-      break;
-     case DVDNAV_NOP:
-      /* Nothing */
+        /* Write output to stdout */
+        fwrite(buf, len, 1, stdout);
       break;
      case DVDNAV_STILL_FRAME: 
        {
-	printf("Skipping still frame\n");
-	dvdnav_still_skip(dvdnav);
-       }
-      break;
-     case DVDNAV_SPU_CLUT_CHANGE:
-       {
-       }
-      break;
-     case DVDNAV_SPU_STREAM_CHANGE:
-       {
-       }
-      break;
-     case DVDNAV_AUDIO_STREAM_CHANGE:
-       {
-       }
-      break;
-     case DVDNAV_HIGHLIGHT:
-       {
-       }
-      break;
-     case DVDNAV_VTS_CHANGE:
-       {
-       }
-      break;
-     case DVDNAV_CELL_CHANGE:
-       {
-	int tt,pt;
-	char ch;
-	
-	dvdnav_current_title_info(dvdnav, &tt, &pt);
-	printf("Cell change... (title, chapter) = (%i,%i)\n", tt,pt);
-
-	if(output_size > (unsigned long)1073741824) {
-	  /* If written more than a gig */
-	  change_output = 1;
-	}
-
-	if(tt_dump != -2) {
-	  if(tt != tt_dump) {
-	    tt_dump = -2;
-	    dump = 0;
-	  } else {
-	    printf("Appending as still title %i\n",tt);
-	  }
-	} else {
-	  dump = 0;
-	  ch = 0;
-	  
-	  fflush(stdin);
-	  while((ch != 'a') && (ch != 's') && (ch != 'q') && (ch != 't')) {
-	    printf("(a)ppend cell to output/(s)kip cell/append until end of (t)itle/(q)uit? (a/s/t/q): ");
-	    scanf("%c", &ch);
-	  }
-	  
-	  if(ch == 'a') {
-	    dump = 1;
-	  }
-	  if(ch == 'q') {
-	    finished = 1;
-	  }
-	  if(ch == 't') {
-	    tt_dump = tt;
-	  }
-	}
-       }
-      break;
-     case DVDNAV_SEEK_DONE:
-       {
-       }
-      break;
-     case DVDNAV_NAV_PACKET:
-       {
-	pci_t pci;
-	dsi_t dsi;
-	
-	/* Take a look at the NAV packet to see if there
-	 * is a menu */
-	if(check_packet(buf, &pci, &dsi)) {
-	  if(pci.hli.hl_gi.btn_ns > 0) {
-	    int button;
-	    
-	    printf("Found %i buttons...\n", pci.hli.hl_gi.btn_ns);
-
-	    for(button = 0; button<pci.hli.hl_gi.btn_ns; button++) {
-	      btni_t *btni;
-
-	      btni = &(pci.hli.btnit[button]);
-	      printf("Button %i top-left @ (%i,%i), bottom-right @ (%i,%i)\n", 
-		     button+1, btni->x_start, btni->y_start,
-		     btni->x_end, btni->y_end);
-	      
-	    }
-
-	    button = 0;
-	    while((button <= 0) || (button > pci.hli.hl_gi.btn_ns)) {
-	      printf("Which button (1 to %i): ", pci.hli.hl_gi.btn_ns);
-	      scanf("%i", &button);
-	    }
-
-	    printf("Selecting button %i\n", button);
-	    dvdnav_button_select_and_activate(dvdnav, button);
-	  }
-	}
+        fprintf(stderr, "Skipping still frame\n");
+        dvdnav_still_skip(dvdnav);
        }
       break;
      case DVDNAV_STOP:
        {
-	finished = 1;
+        finished = 1;
        }
      default:
-      printf("Unknown event (%i)\n", event);
+      fprintf(stderr, "Unhandled event (%i)\n", event);
       finished = 1;
       break;
     }
   }
   
-  CALL(dvdnav_close(dvdnav));
-  close(output_fd);
+  dvdnav_close(dvdnav);
   
   return 0;
 } 
 \endverbatim
 
-Return to the \ref tutorial.
+If you have correctly installled <tt>libdvdnav</tt>, you should have the
+command 'dvdnav-config' in your path. If so you can compile this program
+with
+\verbatim
+  gcc -o dvdtest dvdtest.c `dvdnav-config --cflags --libs`
+\endverbatim
+
+If all goes well, this should generate the 'dvdtest' program in your current working
+directory. You can now start saving a MPEG 2 stream directly off your DVD
+with
+\verbatim
+  ./dvdtest 2>error.log >out.mpeg
+\endverbatim
+
+If the command fails, check the error.log file for details.
+
+\section walkthrorugh Line-by-line walk through
+
+\verbatim
+ include <stdio.h>
+ include <dvdnav/dvdnav.h>
+ include <dvdnav/dvdnav_events.h>
+ include <sys/types.h>
+\endverbatim
+
+These lines include the necessary headers. Almost all <tt>libdvdnav</tt> programs
+will only need to include the dvdnav.h and dvdnav_events.h header files from
+the dvdnav directory.
+
+\verbatim
+ dvdnav_open(&dvdnav, "/dev/dvd");
+\endverbatim
+
+The <tt>libdvdnav</tt> uses <tt>libdvdread</tt> for its DVD I/O. <tt>libdvdread</tt>
+accesses the DVD-device directly so dvdnav_open() needs to be passed the location
+of the DVD device. <tt>libdvdread</tt> can also open DVD images/mounted DVDs. Read
+the <tt>libdvdread</tt> documentation for more information.
+
+\verbatim
+ int result = dvdnav_get_next_block(dvdnav, buf,
+                                    &event, &len);
+\endverbatim
+
+Return to \ref tutorial.
 
 */
