ab0aa87759729cb56cefb4160709b4bbbc340244
tchamp <tchamp@625a9de0-492d-0410-920c-a7e632af1907>
2003-05-06 02:28:28 +0000

    Fixed the link order again and changed a return value to match better with Win32
    git-svn-id: svn://svn.mplayerhq.hu/dvdnav/trunk/libdvdnav@713 625a9de0-492d-0410-920c-a7e632af1907

--- msvc/scripts/libdvdnav_install.bat
+++ msvc/scripts/libdvdnav_install.bat
@@ -9,6 +9,7 @@ ECHO includes ...
 xcopy /Y ..\src\dvd_reader.h install\include\
 xcopy /Y ..\src\ifo_types.h install\include\dvdnav
 xcopy /Y ..\src\ifo_read.h install\include\dvdnav
+xcopy /Y ..\src\dvdnav.h install\include\dvdnav
 
 ECHO lib ...
 xcopy /Y %1\libdvdnav\libdvdnav.lib install\lib
