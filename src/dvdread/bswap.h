c6b6a57ade5f63020fc21247a91012c02696905f
nicodvb <nicodvb@625a9de0-492d-0410-920c-a7e632af1907>
2007-05-05 10:32:13 +0000

    define B2xx macros for BEOS as for WIN32 variants; patch by Videolan team
    git-svn-id: svn://svn.mplayerhq.hu/dvdnav/trunk/libdvdnav@935 625a9de0-492d-0410-920c-a7e632af1907

--- src/dvdread/bswap.h
+++ src/dvdread/bswap.h
@@ -71,7 +71,7 @@
  * functionality! 
  */
 
-#elif defined(__FreeBSD__) || defined(__sun) || defined(__bsdi__) || defined(WIN32) || defined(__CYGWIN__)
+#elif defined(__FreeBSD__) || defined(__sun) || defined(__bsdi__) || defined(WIN32) || defined(__CYGWIN__) || defined(__BEOS__)
 #define B2N_16(x) \
  x = ((((x) & 0xff00) >> 8) | \
       (((x) & 0x00ff) << 8))
