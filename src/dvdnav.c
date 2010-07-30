2f1217ceab17988a0bd8b7aa7df12e7ee6609542
erik <erik@625a9de0-492d-0410-920c-a7e632af1907>
2010-07-30 23:34:08 +0000

    Clear all of position_current
    In dvdnav_clear the only thing cleared of position_current is
    one member. Instead clear the whole thing.
    
    This is derived from a patch submitted by John Stebbins. Thanks!
    
    git-svn-id: svn://svn.mplayerhq.hu/dvdnav/trunk/libdvdnav@1205 625a9de0-492d-0410-920c-a7e632af1907

--- src/dvdnav.c
+++ src/dvdnav.c
@@ -52,12 +52,12 @@ static dvdnav_status_t dvdnav_clear(dvdnav_t * this) {
   if (this->file) DVDCloseFile(this->file);
   this->file = NULL;
 
+  memset(&this->position_current,0,sizeof(this->position_current));
   memset(&this->pci,0,sizeof(this->pci));
   memset(&this->dsi,0,sizeof(this->dsi));
   this->last_cmd_nav_lbn = SRI_END_OF_CELL;
 
   /* Set initial values of flags */
-  this->position_current.still = 0;
   this->skip_still = 0;
   this->sync_wait = 0;
   this->sync_wait_skip = 0;
