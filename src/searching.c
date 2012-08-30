cb293e4a2ab4fa8aa99e9fb4bec6db38eb639110
Erik Hovland <ehovland@s383.jpl.nasa.gov>
2012-08-30 14:27:32 -0700

    Make sure pgc is valid before dereferencing.
    The pgc pointer might be null when it is retrieved from the IFO. This
    can cause a segfault when pgc is dereferenced a few lines later.

--- src/searching.c
+++ src/searching.c
@@ -635,6 +635,10 @@ uint32_t dvdnav_describe_title_chapters(dvdnav_t *this, int32_t title, uint64_t 
   for(i=0; i<parts; i++) {
     uint32_t cellnr, endcellnr;
     pgc = ifo->vts_pgcit->pgci_srp[ptt[i].pgcn-1].pgc;
+    if (pgc == NULL) {
+      printerr("PGC missing.");
+      continue;
+    }
     if(ptt[i].pgn > pgc->nr_of_programs) {
       printerr("WRONG part number.");
       goto fail;
