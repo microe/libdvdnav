microe/libdvdnav

commit 1bfa0ec8c9812fd22faae318011e24874fb5d3ba

Fix failed assertion when there's no menu.
Patch by Erik Hovland.


git-svn-id: svn://svn.mplayerhq.hu/dvdnav/trunk/libdvdnav@1245 625a9de0-492d-0410-920c-a7e632af1907

src/vm/vm.c
@@ -1762,7 +1762,8 @@ static int set_PGCN(vm_t *vm, int pgcN) {
   pgcit_t *pgcit;
 
   pgcit = get_PGCIT(vm);
-  assert(pgcit != NULL);  /* ?? Make this return -1 instead */
+  if (pgcit != NULL)
+    return 0;
 
   if(pgcN < 1 || pgcN > pgcit->nr_of_pgci_srp) {
 #ifdef TRACE

commit 1a97eea0c41e995e1a2a310ac74f33ee9b5396b7

Unbreak playback of DVDs with menus, broken by r1245.
Patch by Erik Auerswald \auerswal % unix-ag * uni-kl * de/.


git-svn-id: svn://svn.mplayerhq.hu/dvdnav/trunk/libdvdnav@1251 625a9de0-492d-0410-920c-a7e632af1907

src/vm/vm.c
@@ -1762,7 +1762,7 @@ static int set_PGCN(vm_t *vm, int pgcN) {
   pgcit_t *pgcit;
 
   pgcit = get_PGCIT(vm);
-  if (pgcit != NULL)
+  if (pgcit == NULL)
     return 0;
 
   if(pgcN < 1 || pgcN > pgcit->nr_of_pgci_srp) {